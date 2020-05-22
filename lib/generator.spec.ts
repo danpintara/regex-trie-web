dc108c7cdcbfa54703990f92364e03aa50e9ff87
Daniel Pintara <danpintara@gmail.com>
2020-05-22 18:06:41 +0700

    feat(lib): Generator returns string when input is single

--- lib/generator.spec.ts
+++ lib/generator.spec.ts
@@ -1,5 +1,11 @@
+import { lorem } from "faker"
 import { generate } from "./generator"
 
 test("generate empty returns undefined", () => {
   expect(generate([])).toBe(undefined)
+})
+
+test("generate a word returns word", () => {
+  const word = lorem.word()
+  expect(generate([word])).toBe(word)
 })
