1c88a716c7d044acf5140048a5bb69ddfb82f304
Daniel Pintara <danpintara@gmail.com>
2020-05-23 03:20:14 +0700

    test(lib): Rename tests on generator

--- lib/generator.spec.ts
+++ lib/generator.spec.ts
@@ -2,16 +2,16 @@ import { lorem } from "faker"
 import RegexTrie from "regex-trie"
 import { generate } from "./generator"
 
-test("generate empty returns undefined", () => {
+test("generate undefined regexp. when input is undefined", () => {
   expect(generate([])).toBe(undefined)
 })
 
-test("generate a word returns word", () => {
+test("generate single word regexp. when input is a word", () => {
   const word = lorem.word()
   expect(generate([word])).toBe(word)
 })
 
-test("generate input returns regular expression", () => {
+test("generate regexp. from input", () => {
   const words = lorem.words().split(" ")
   const trie = RegexTrie()
   words.forEach((word) => trie.add(word))
@@ -19,7 +19,7 @@ test("generate input returns regular expression", () => {
   expect(generate(words)).toBe(trie.toString())
 })
 
-test("generate regular expression with capturing group if configured", () => {
+test("generate regexp. with capturing group if configured", () => {
   const prefix = lorem.word()
   const words = lorem
     .words()
