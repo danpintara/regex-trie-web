505a8c4116cb109beee362b70defca9fd6e9915b
Daniel Pintara <danpintara@gmail.com>
2020-05-24 22:14:49 +0700

    test(components): Changes on TextArea reflect to onChange callback

--- components/TextInput.spec.tsx
+++ components/TextInput.spec.tsx
@@ -1,10 +1,20 @@
-import { shallow } from "enzyme"
+import { mount, shallow } from "enzyme"
 import { lorem } from "faker"
 import React from "react"
+import { spy } from "sinon"
 import TextInput from "./TextInput"
 
 test("set TextArea value", () => {
   const value = lorem.words()
   const dom = shallow(<TextInput value={value} onChange={() => undefined} />)
   expect(dom.first().prop("value")).toEqual(value)
+})
+
+test("TextArea onChange callback", () => {
+  const value = lorem.words()
+  const onChangeCallback = spy()
+  mount(<TextInput value={""} onChange={onChangeCallback} />)
+    .first()
+    .simulate("change", { target: { value } })
+  expect(onChangeCallback.calledWith(value)).toBeTruthy()
 })
