5327a596e253584371a8f7378a9c69efab05b143
Daniel Pintara <danpintara@gmail.com>
2020-06-01 04:52:37 +0700

    feat: Add "Fork me on GitHub" ribbon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Space, Switch } from "antd"
 import React, { useState } from "react"
+import GitHubForkRibbon from "react-github-fork-ribbon"
 import Header from "../components/Header"
 import TextInput from "../components/TextInput"
 import { generate } from "../lib/generator"
@@ -9,32 +10,40 @@ function HomePage() {
   const [useNonCapturingGroup, setUseNonCapturingGroup] = useState(true)
 
   return (
-    <Space
-      direction="vertical"
-      size="large"
-      style={{
-        alignItems: "center",
-        display: "flex",
-        padding: 32,
-      }}
-    >
-      <Header />
-      <div>Input:</div>
-      <TextInput value={value} onChange={setValue} />
-      <div style={{ display: "flex", justifyItems: "center" }}>
-        Use non-capturing group&nbsp;&nbsp;
-        <Switch
-          checked={useNonCapturingGroup}
-          onChange={(val) => setUseNonCapturingGroup(val)}
-        />
-      </div>
-      <div>Output:</div>
-      <div>
-        {generate(value.split("\n"), useNonCapturingGroup) ?? (
-          <i>(no result)</i>
-        )}
-      </div>
-    </Space>
+    <React.Fragment>
+      <GitHubForkRibbon
+        href="https://github.com/danpintara/regex-trie-web"
+        target="_blank"
+      >
+        Fork me on GitHub
+      </GitHubForkRibbon>
+      <Space
+        direction="vertical"
+        size="large"
+        style={{
+          alignItems: "center",
+          display: "flex",
+          padding: 32,
+        }}
+      >
+        <Header />
+        <div>Input:</div>
+        <TextInput value={value} onChange={setValue} />
+        <div style={{ display: "flex", justifyItems: "center" }}>
+          Use non-capturing group&nbsp;&nbsp;
+          <Switch
+            checked={useNonCapturingGroup}
+            onChange={(val) => setUseNonCapturingGroup(val)}
+          />
+        </div>
+        <div>Output:</div>
+        <div>
+          {generate(value.split("\n"), useNonCapturingGroup) ?? (
+            <i>(no result)</i>
+          )}
+        </div>
+      </Space>
+    </React.Fragment>
   )
 }
 
