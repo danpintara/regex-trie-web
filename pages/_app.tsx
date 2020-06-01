5c622b2efdf073df7f65a38e8d4a4e03d9b48409
Daniel Pintara <danpintara@gmail.com>
2020-06-02 00:36:47 +0700

    refactor(pages): Move GitHub fork ribbon into _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
-import React from "react"
-import App from "next/app"
-
 import "antd/dist/antd.css"
+import App from "next/app"
+import React from "react"
+import GitHubForkRibbon from "react-github-fork-ribbon"
+
+function ForkRibbon() {
+  return (
+    <GitHubForkRibbon
+      href="https://github.com/danpintara/regex-trie-web"
+      target="_blank"
+    >
+      Fork me on GitHub
+    </GitHubForkRibbon>
+  )
+}
 
 class MainApp extends App {
   render() {
     const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
+    return (
+      <React.Fragment>
+        <ForkRibbon />
+        <Component {...pageProps} />
+      </React.Fragment>
+    )
   }
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { Space, Switch } from "antd"
 import React, { useState } from "react"
-import GitHubForkRibbon from "react-github-fork-ribbon"
 import Header from "../components/Header"
 import TextInput from "../components/TextInput"
 import { generate } from "../lib/generator"
@@ -11,12 +10,6 @@ function HomePage() {
 
   return (
     <React.Fragment>
-      <GitHubForkRibbon
-        href="https://github.com/danpintara/regex-trie-web"
-        target="_blank"
-      >
-        Fork me on GitHub
-      </GitHubForkRibbon>
       <Space
         direction="vertical"
         size="large"
