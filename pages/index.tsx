a9168b2cd9433373864e4b4577cd175559ead941
Daniel Pintara <danpintara@gmail.com>
2020-06-02 00:52:33 +0700

    refactor(pages): Remove unused Fragment in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,32 +8,30 @@ function HomePage() {
   const [useNonCapturingGroup, setUseNonCapturingGroup] = useState(true)
 
   return (
-    <React.Fragment>
-      <Space
-        direction="vertical"
-        size="large"
-        style={{
-          alignItems: "center",
-          display: "flex",
-        }}
-      >
-        <div>Input:</div>
-        <TextInput value={value} onChange={setValue} />
-        <div style={{ display: "flex", justifyItems: "center" }}>
-          Use non-capturing group&nbsp;&nbsp;
-          <Switch
-            checked={useNonCapturingGroup}
-            onChange={(val) => setUseNonCapturingGroup(val)}
-          />
-        </div>
-        <div>Output:</div>
-        <div>
-          {generate(value.split("\n"), useNonCapturingGroup) ?? (
-            <i>(no result)</i>
-          )}
-        </div>
-      </Space>
-    </React.Fragment>
+    <Space
+      direction="vertical"
+      size="large"
+      style={{
+        alignItems: "center",
+        display: "flex",
+      }}
+    >
+      <div>Input:</div>
+      <TextInput value={value} onChange={setValue} />
+      <div style={{ display: "flex", justifyItems: "center" }}>
+        Use non-capturing group&nbsp;&nbsp;
+        <Switch
+          checked={useNonCapturingGroup}
+          onChange={(val) => setUseNonCapturingGroup(val)}
+        />
+      </div>
+      <div>Output:</div>
+      <div>
+        {generate(value.split("\n"), useNonCapturingGroup) ?? (
+          <i>(no result)</i>
+        )}
+      </div>
+    </Space>
   )
 }
 
