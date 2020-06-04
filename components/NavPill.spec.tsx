d7a5e617aeb34ea13c28cf2d4db0a4a1106e3f55
Daniel Pintara <danpintara@gmail.com>
2020-06-04 23:53:20 +0700

    feat(components): Empty NavPill component

---
+++ components/NavPill.spec.tsx
@@ -0,0 +1,7 @@
+import { shallow } from "enzyme"
+import React from "react"
+import NavPill from "./NavPill"
+
+test("Render", () => {
+  shallow(<NavPill />)
+})
---
+++ components/NavPill.tsx
@@ -0,0 +1,3 @@
+export default function NavPill() {
+  return null
+}
