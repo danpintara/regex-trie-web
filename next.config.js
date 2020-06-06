0a59d37949e24950547b9934a65d1b3f1f4c3126
Daniel Pintara <danpintara@gmail.com>
2020-06-06 20:04:40 +0700

    feat(pages): Content of About page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+})
+
+module.exports = withMDX({
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
   experimental: {
     basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
   },
-}
+})
--- tests/pages/about.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { shallow } from "enzyme"
-import React from "react"
-import AboutPage from "../../pages/about"
-
-test("render", () => {
-  shallow(<AboutPage />)
-})
