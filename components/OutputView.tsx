5f022f4ed3350c084ccadc903455d10d7c2da513
Daniel Pintara <danpintara@gmail.com>
2020-06-07 01:26:28 +0700

    feat(components): Use lighter placeholder color in OutputView

--- components/OutputView.tsx
+++ components/OutputView.tsx
@@ -9,7 +9,7 @@ const OutputView: React.FunctionComponent<Props> = function (props) {
   return (
     <div>
       {props.value ?? (
-        <span style={{ color: "#5e5e5e" }}>{props.sampleValue}</span>
+        <span style={{ color: "#b0b0b0" }}>{props.sampleValue}</span>
       )}
     </div>
   )
