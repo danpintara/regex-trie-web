15ec89243a3b177f3f117ea63df2d4fb6737c026
Daniel Pintara <danpintara@gmail.com>
2020-06-07 08:21:55 +0700

    feat(components): Additional style in OutputView

--- components/OutputView.tsx
+++ components/OutputView.tsx
@@ -7,11 +7,23 @@ interface Props {
 
 const OutputView: React.FunctionComponent<Props> = function (props) {
   return (
-    <div>
-      {props.value ?? (
-        <span style={{ color: "#b0b0b0" }}>{props.sampleValue}</span>
-      )}
-    </div>
+    <pre className={props.value ? null : "sample"}>
+      {props.value ?? props.sampleValue}
+      <style jsx>{`
+        pre {
+          padding: 8px;
+          background-color: #f0f0f0;
+          width: 300px;
+          max-width: 400px;
+          min-height: 200px;
+          white-space: pre-wrap;
+          overflow-wrap: break-word;
+        }
+        .sample {
+          color: #b0b0b0;
+        }
+      `}</style>
+    </pre>
   )
 }
 
