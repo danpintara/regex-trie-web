4fc562f353fddbc020aecaa8ecde4526c6ab9f8f
Daniel Pintara <danpintara@gmail.com>
2020-06-08 14:52:54 +0700

    test: Fix selector in TextInput

--- components/TextInput.spec.tsx
+++ components/TextInput.spec.tsx
@@ -1,3 +1,4 @@
+import { Input } from "antd"
 import { mount, shallow } from "enzyme"
 import { lorem } from "faker"
 import React from "react"
@@ -7,13 +8,14 @@ import TextInput from "./TextInput"
 test("set TextArea value", () => {
   const value = lorem.words()
   const dom = shallow(<TextInput value={value} onChange={() => undefined} />)
-  expect(dom.first().prop("value")).toEqual(value)
+  expect(dom.find(Input.TextArea).first().prop("value")).toEqual(value)
 })
 
 test("TextArea onChange callback", () => {
   const value = lorem.words()
   const onChangeCallback = spy()
   mount(<TextInput value={""} onChange={onChangeCallback} />)
+    .find(Input.TextArea)
     .first()
     .simulate("change", { target: { value } })
   expect(onChangeCallback.calledWith(value)).toBeTruthy()
@@ -22,5 +24,7 @@ test("TextArea onChange callback", () => {
 test("Sample value", () => {
   const sampleValue = lorem.words()
   const dom = shallow(<TextInput sampleValue={sampleValue} />)
-  expect(dom.first().prop("placeholder")).toEqual(sampleValue)
+  expect(dom.find(Input.TextArea).first().prop("placeholder")).toEqual(
+    sampleValue
+  )
 })
